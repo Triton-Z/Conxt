60f4fa8d97536e74eb79d7cc3deb17516daace9b
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-03-29 11:55:03 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,16 @@
 var x = 1;
 var y = 1;
+var backgroundTxt = document.getElementsByClassName("span");
 var player = document.getElementById("x"+x+"y"+y);
 function start (){
 player = document.getElementById("x"+x+"y"+y);
 player.innerHTML="#";
 x++;
 y++;
+}
+
+function replaceContent() {
+for (var i = 0; i<backgroundTxt.length; i++) {
+backgroundTxt[i].innerHTML = document.getElementById("inputField").value;
+
 }
