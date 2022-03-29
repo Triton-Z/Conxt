eece7b6256d2c7bdef807cfa34e6402e9e641b05
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-03-29 11:35:05 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@ var x = 1;
 var y = 1;
 var player = document.getElementById("x"+x+"y"+y);
 function start (){
-x++;
-y++;
 player = document.getElementById("x"+x+"y"+y);
 player.innerHTML="#";
+x++;
+y++;
 }
