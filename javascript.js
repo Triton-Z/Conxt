cd516e0d3b834f2784e4c4fac2366141ebe3e2cd
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-03-30 11:03:04 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,30 @@
+//variable storage
 var x = 1;
 var y = 1;
 var backgroundTxt = document.getElementsByClassName("textCharacters");
 var player = document.getElementById("x"+x+"y"+y);
-function start (){
+
+function move (direction){
 player = document.getElementById("x"+x+"y"+y);
 player.innerHTML="#";
-x++;
+//1 is up
+//2 is right
+//3 is down
+//4 is left
+
+if (direction==1) {
 y++;
+} else if (direction==2) {
+x++;
+} else if (direction==3) {
+y=y-1;
+} else if (direction = 4) {
+x=x-1;
 }
+
+
+} //function ending, don't get confused
+
 
 function replaceContent() {
 for (var i = 0; i<backgroundTxt.length; i++) {
