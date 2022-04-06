878e4541381531b86daa88dd29a17c5f4ed9075c
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-04-06 13:15:49 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 //variable storage
 var x = 1;
 var y = 1;
+var characterType = "0";
 var backgroundTxt = document.getElementsByClassName("textCharacters");
 var player = document.getElementById("x"+x+"y"+y);
-
+var remove = document.getElementById("x"+x+"y"+y);
 function move (direction){
 //1 is up
 //2 is right
@@ -29,6 +30,25 @@ function replaceContent() {
 for (var i = 0; i<backgroundTxt.length; i++) {
 backgroundTxt[i].innerHTML = document.getElementById("inputField").value;
   }
+}
+
+function remove(direction, move)
+
+if (direction==1) {
+//up
+
+} else if (direction==2) {
+//right
+	
+} else if (direction==3) {
+//down
+	
+} else if (direction==4) {
+//left
+
+}
+
+remove.innerHTML = 
 }
 
 window.onload = function(){
